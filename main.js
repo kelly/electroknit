054c63bfca439dab524af64c2b2512a0d2d9478d
kelly <korevec@gmail.com>
2016-01-10 18:02:55 -0800

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Machine = require('./lib/machine');
+const Pattern = require('./lib/pattern');
+
+const Electroknit = {
+	Machine: Machine,
+	Pattern: Pattern
+}
+
+module.exports = Electroknit;
